9e471e70a0cb7095725b4cdc2d1de1807da4ecf6
Eren Erden <studentzboytwo@gmail.com>
2023-05-21 14:47:49 +0300

    sayaçç.py added

--- sayaçç.py
+++ sayaçç.py
@@ -1,15 +1,15 @@
 import time
 from plyer import notification
-def countdown(n): # sonsuz döngü içinde n değişkeni 0 a eşit değilse n değişkeninden 1 çıkar ve 1 saniye bekle
+def countdown(n): 
     while n != 0:
         n -= 1
-        time.sleep(1) #bir saniyeliğine programı durdur/uyut/kapat
-        print(n) #her n değişkeni azalınca ve 1 saniye geçince n-1 i yani geri sayım süresini göster
+        time.sleep(1) 
+        print(n) 
     notification.notify(
-        title = "sayacın geri bildirimi", #geri bildirimin başlık kısmı
-        message = "ZAMAN DOLDU ASKER!! ", #geri bildirim mesajı
-        timeout=2,) #2 saniye olunca while döngüsü ile beraber bitir programı
-    time.sleep(5) #eğer yazmazsanız mesaj otomatik olarak değil manuel olarak kapatılır ve parantezin dışına yazılmalı
+        title = "sayacın geri bildirimi", 
+        message = "ZAMAN DOLDU ASKER!! ", 
+        timeout=2,) 
+    time.sleep(5)
 
 
 sayac = int(input("geri sayım için zaman dilimi giriniz:  "))
